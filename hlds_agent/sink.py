f9d43fe81067b3e4d90fede02c19c69a31fed91b
Sean Reed <reeds@valassis.com>
2018-04-22 17:28:58 -0700

    Add snappy

--- hlds_agent/sink.py
+++ hlds_agent/sink.py
@@ -1,6 +1,9 @@
+import base64
+import json
 import time
 
 import requests
+import snappy
 
 from elasticsearch import Elasticsearch
 
@@ -28,6 +31,8 @@ class ElasticSearchSink(Sink):
             res = self.es.index(index=todaysIndex, doc_type=blob['type'], body=blob)
             print(res['_id'] + ': ' + repr(blob))
 
+        return True
+
 
     def get_index(self):
         day = time.strftime("%d-%m-%Y")
@@ -37,8 +42,29 @@ class ElasticSearchSink(Sink):
 
 class HttpSink(Sink):
 
-    def __init__(self, auth_token, host):
+    def __init__(self, host, auth_token=None):
         self.host = host
+        self.auth_token = auth_token
+
 
     def send(self, blobs):
-        pass
+        headers = self._headers()
+        compressed_blob = self._compress(blobs)
+        response = requests.post(self.host, headers=headers, data={'blob': compressed_blob})
+
+        if response.status_code == 200:
+            return True
+        else:
+            response.raise_for_status()
+
+    def _compress(self, blobs):
+        json_blobs = json.dumps(blobs)
+        json_blobs = json_blobs.encode('utf-8')
+        compressed_blobs = snappy.compress(json_blobs)
+        return base64.b64encode(compressed_blobs)
+
+
+    def _headers(self):
+        return {'X-HLDS-AUTH': self.auth_token,
+                'Content-Type': 'application/json'}
+
